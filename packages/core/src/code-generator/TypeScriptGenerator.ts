88127eb1b2ba30bcdea18ced6115847ddf13c5cb
bughunter-jt <jeongtaek2@hotmail.com>
2026-07-02 16:23:03 +0100

    feat: add TypeScriptGenerator

---
+++ packages/core/src/code-generator/TypeScriptGenerator.ts
@@ -0,0 +1,31 @@
+import { CodeGenerator, GenerateOptions } from "./code-generator";
+import { StateMachine } from "../ir/state-machine";
+
+export class TypeScriptGenerator implements CodeGenerator<string> {
+  language = "typescript";
+
+  generate(machine: StateMachine, options?: GenerateOptions): string {
+    const states = Object.entries(machine.states)
+      .map(([stateName, state]) => {
+        const transitions = state.transitions
+          .map((t) => `        ${t.event}: "${t.target}"`)
+          .join(",\n");
+
+        return `
+    ${stateName}: {
+      on: {
+${transitions}
+      }
+    }`;
+      })
+      .join(",\n");
+
+    return `
+export const ${machine.name.toLowerCase()}Machine = {
+  initial: "${machine.initialState}",
+  states: {${states}
+  }
+} as const;
+`;
+  }
+}
