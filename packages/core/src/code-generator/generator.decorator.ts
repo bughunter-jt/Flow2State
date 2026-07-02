ed49b963b798ab00316ddc020246f4424fc71c9c
bughunter-jt <jeongtaek2@hotmail.com>
2026-07-02 16:50:07 +0100

    feat: add decorator for generator

---
+++ packages/core/src/code-generator/generator.decorator.ts
@@ -0,0 +1,10 @@
+import { registry } from "./GeneratorRegistry";
+import { CodeGenerator } from "./code-generator";
+
+type Constructor<T = any> = new (...args: any[]) => T;
+
+export function Generator(name: string) {
+  return function <T extends CodeGenerator>(target: Constructor<T>) {
+    registry.register(name, new target());
+  };
+}
